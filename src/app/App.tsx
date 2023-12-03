1ff0c2b556669ad8e1e589f99cd1aed62c1ddb6e
Jaslan <jaslan.dlnv@gmail.com>
2023-12-03 15:45:58 +0300

    refactor: add routeConfig, update AppRouter and App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,20 @@
-import { Suspense } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./styles/index.scss";
 
-import { MainPage } from "../pages/MainPage";
-import { AboutPage } from "../pages/AboutPage";
 import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
 import { className } from "../shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={className("app", {}, [theme])}>
+            <button onClick={toggleTheme}>Toggle Theme</button>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О Сайте</Link>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Suspense fallback={<h2>Loading...</h2>}>
-                <Routes>
-                    <Route path={"/"} element={<MainPage />} />
-                    <Route path={"/about"} element={<AboutPage />} />
-                </Routes>
-            </Suspense>
+
+            <AppRouter />
         </div>
     );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,21 @@
+import { Suspense, type FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+const AppRouter: FC = props => {
+    return (
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={element}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
+
+export default AppRouter;
