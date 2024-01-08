9ef514502d4f63c439ec1d356ffe2846e506c7f4
Jaslan <jaslan.dlnv@gmail.com>
2024-01-08 20:17:10 +0800

    refactor: add sidebar in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
 import "./styles/index.scss";
 
 import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
 import { classNames } from "../shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
     const { theme } = useTheme();
@@ -12,8 +12,10 @@ const App = () => {
     return (
         <div className={classNames("app", {}, [theme])}>
             <Navbar />
-
-            <AppRouter />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
         </div>
     );
 };
